3fc314fd42496d2c4f818240f03c2e119e709905
chirazseni <vucxdftc@gmail.com>
2026-07-25 06:07:17 +0100

    fix

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// ==================== SERVE FICHIERS STATIQUES (frontend) ====================
 app.use(express.static(__dirname));
 
 // ==================== ROOT ROUTE ====================
